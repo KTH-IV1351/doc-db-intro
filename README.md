656c88e2a0cfd22aed01ff37b9a72b6b402b0f54
Leif <leifl@kth.se>
2020-11-12 17:17:52 +0100

    now lists db, col and doc

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# doc-db-intro
+# Introduction to Document Databases and MongoDB
 
 This application illustrates how to use MongoDB from a Java program.
 
-## How to execute.
+## Tutorial
+
+It might be a good idea to follow the tutorial at `https://developer.mongodb.com/quickstart/java-setup-crud-operations`
+
+## How to Execute
 
 1. This application uses a free cluster in MongoDB Atlas. Hot to create such a cluster is explained here `https://developer.mongodb.com/quickstart/free-atlas-cluster`
-1.
+1. You have to set the following environment variables before starting the program.
+  * `MONGODB_URL` The url to which the MongoDB driver will connect. How to find the URL is explained in the Atlas quickstart guide mentioned above.
--- src/main/java/se/kth/iv1351/App.java
+++ src/main/java/se/kth/iv1351/App.java
@@ -23,10 +23,48 @@
 
 package se.kth.iv1351;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import static com.mongodb.client.model.Filters.eq;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+    private static final String DB_NAME = "sample_training";
+    private static final String COLLECTION_NAME = "grades";
+    private static final int SEARCH_EX_FIELD_VALUE = 5;
+    private static final String SEARCH_EX_FIELD_NAME = "student_id";
+
+    public static void main(String[] args) {
+        String dbUrl = System.getenv("MONGODB_URL");
+        try (MongoClient connection = MongoClients.create(dbUrl)) {
+
+            LOGGER.trace("Databases:");
+            for (Document database : connection.listDatabases()) {
+                LOGGER.trace(database.toJson());
+            }
+            LOGGER.trace("\n");
+
+            LOGGER.trace("Collections in " + DB_NAME + ":");
+            MongoDatabase trainingDb = connection.getDatabase(DB_NAME);
+            for (Document collection : trainingDb.listCollections()) {
+                LOGGER.trace(collection.toJson());
+            }
+            LOGGER.trace("\n");
+
+            LOGGER.trace("Documents in " + COLLECTION_NAME + " for " + SEARCH_EX_FIELD_NAME 
+                         + " = " + SEARCH_EX_FIELD_VALUE + ":");
+            MongoCollection<Document> grades = trainingDb.getCollection(COLLECTION_NAME);
+            for (Document doc : grades.find(eq(SEARCH_EX_FIELD_NAME, SEARCH_EX_FIELD_VALUE))) {
+                LOGGER.trace(doc.toJson());
+            }
+            LOGGER.trace("\n");
+        }
     }
 }
